cda94e1d77380e9f5c9221c0e77e6ec8516a9061
github-usman <usmantoyou@gmail.com>
2023-08-11 13:07:13 +0530

    adding MongoDB and mongoose

--- backend/Blog-with-Database-Starting-Files/app.js
+++ backend/Blog-with-Database-Starting-Files/app.js
@@ -4,6 +4,7 @@ import express from "express";
 import bodyParser from "body-parser";
 import ejs from "ejs";
 import _  from 'lodash';
+import mongoose from "mongoose";
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -22,13 +23,34 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 
-let posts = [];
+// let posts = [];
+// connect to mongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/test');
+// schema
+const post = new mongoose.Schema({
+  title: String,
+  message: String
+
+});
+
+const Blog = mongoose.model('Blog',post);
+
+const Day_one = new Blog({
+  title:'One Day',
+  message:"honcus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui."
+})
+
+// Day_one.save();
+
+console.log(Day_one.title);
+
+
 
 const year = new Date().getFullYear();
 let userTitle = "";
 let userMessage = ""
 app.get("/", (req, res) => {
-  res.render("./home", { homeContent: aboutContent,year:year,posts:posts});
+  res.render("./home", { homeContent: aboutContent,year:year,posts:Day_one});
  
 });
 
@@ -47,15 +69,24 @@ app.get("/compose", (req, res) => {
 
 app.post('/compose', (req, res) => {
 
-   const post = {
-      title : req.body.titleInput,
-      message : req.body.postInput
+  //  const post = {
+  //     title : req.body.titleInput,
+  //     message : req.body.postInput
 
-   } 
+  //  } 
+    if(req.body.titleInput !== ""){
+      const Day_one = new Blog({
+        title:req.body.titleInput,
+        message:req.body.postInput
+      })
+
+      Day_one.save();
+    }
   
-      posts.push(post);
-  // console.log(userInput.message);
-  // res.render("./home", { homeContent: homeStartingContent,year:year,userTitle:userInput.title,userMessage:userInput.message});
+   
+  
+      // posts.push(post);
+     
   res.redirect('/');
 });
 
@@ -63,19 +94,21 @@ app.post('/compose', (req, res) => {
 // dynamic URL
 
 app.get("/posts/:topic",(req,res)=>{
-   
+  console.log("not Matched");
 
-  posts.forEach((post)=>{
+  // posts.forEach((post)=>{
      
-    if(_.lowerCase(req.params.topic) === _.lowerCase(post.title)){
+    if(_.lowerCase(req.params.topic) === _.lowerCase(Day_one.title)){
       // console.log("matched");
       res.render("./partials/post", {
-         title:post.title,
-         content:post.message,
+         title:Day_one.title,
+         content:Day_one.message,
          year:year
         });  
+    }else{
+      console.log("not Matched");
     }
-  });
+  // });
  
   res.redirect("/posts");
 });
